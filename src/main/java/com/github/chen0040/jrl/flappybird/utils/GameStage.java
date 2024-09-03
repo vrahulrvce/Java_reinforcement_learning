b0c8dc6d7717171d30d3271e376aad4cdc8f48aa
Rahul <86185777+vrahulrvce@users.noreply.github.com>
2024-09-02 21:34:33 -0400

    Delete src/main directory

--- src/main/java/com/github/chen0040/jrl/flappybird/utils/GameStage.java
+++
@@ -1,6 +0,0 @@
-package com.github.chen0040.jrl.flappybird.utils;
-
-public enum GameStage {
-    Pending,
-    InProgress
-}
--- src/main/java/com/github/chen0040/jrl/flappybird/utils/IterTools.java
+++
@@ -1,7 +0,0 @@
-package com.github.chen0040.jrl.flappybird.utils;
-
-public class IterTools {
-    public static Cycle cycle(int[] pattern){
-        return new Cycle(pattern);
-    }
-}
